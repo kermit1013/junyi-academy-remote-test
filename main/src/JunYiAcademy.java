8eea731186285da2d8a0b6b29e185755e0981eff
kermit sung <k84101311@gmail.com>
2021-03-11 18:21:51 +0800

    finished removeMultiples method

--- main/src/JunYiAcademy.java
+++ main/src/JunYiAcademy.java
@@ -1,7 +1,13 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class JunYiAcademy {
+
     public static void main(String[] args) {
         System.out.println("Reversed String: " + reverseString("junyiacademy"));
         System.out.println("Reversed Sentence: " + reverseSentence("flipped class room is important"));
+        System.out.println("Removed Multiples and Count: " + removeMultiples(15,3,5).size());
     }
 
     public static String reverseString(String string) {
@@ -18,5 +24,14 @@ public class JunYiAcademy {
             stringBuilder.append(words[i]).append(" ");
         }
         return stringBuilder.toString();
+    }
+
+    public static List<Integer> removeMultiples(int integer, int multiple1, int multiple2) {
+        List<Integer> numbers = Stream.iterate(1, n -> n + 1)
+                                      .limit(integer)
+                                      .filter(number-> number % multiple1 != 0 && number % multiple2 != 0)
+                                      .collect(Collectors.toList());
+        numbers.add(integer);
+        return numbers;
     }
 }
