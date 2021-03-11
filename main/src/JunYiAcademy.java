562deed253af8d077e4419a04ad39afab8f2651c
kermit sung <k84101311@gmail.com>
2021-03-11 18:07:44 +0800

    finished reverseString method

--- main/src/JunYiAcademy.java
+++ main/src/JunYiAcademy.java
@@ -1,5 +1,12 @@
 public class JunYiAcademy {
     public static void main(String[] args) {
-       System.out.println("Hello World");
+        System.out.println("Reversed String: " + reverseString("junyiacademy"));
+    }
+
+    public static String reverseString(String string) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(string);
+        stringBuilder.reverse();
+        return stringBuilder.toString();
     }
 }
